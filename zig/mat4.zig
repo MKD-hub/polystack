f8c4d7588e56d6416696e8766d7c705dda7c3c85
mocca_mo <mkdiab58@gmail.com>
2026-01-04 14:49:40 +0300

    rewrite: Rewrote logic for mat4 and vec4

--- zig/mat4.zig
+++ zig/mat4.zig
@@ -1,28 +1,44 @@
-const vec3 = @import("vec3.zig").Vec3;
+const Vec4 = @import("vec4.zig").Vec4;
 
 pub const Mat4 = struct {
-    elements: [16]f32,
+    colx: Vec4,
+    coly: Vec4,
+    colz: Vec4,
+    col3: Vec4,
 
     pub fn init() Mat4 {
-        return Mat4{ .elements = [_]f32{0} ** 16 };
+        return .{
+            .colx = Vec4.init{ 0, 0, 0, 0 },
+            .coly = Vec4.init{ 0, 0, 0, 0 },
+            .colz = Vec4.init{ 0, 0, 0, 0 },
+            .col3 = Vec4.init{ 0, 0, 0, 0 },
+        };
     }
 
     pub fn identity() Mat4 {
-        return Mat4{ .elements = [_]f32{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 } };
+        return .{
+            .colx = Vec4.init{ 1, 0, 0, 0 },
+            .coly = Vec4.init{ 0, 1, 0, 0 },
+            .colz = Vec4.init{ 0, 0, 1, 0 },
+            .col3 = Vec4.init{ 0, 0, 0, 1 },
+        };
     }
 
-    pub fn multiplyMat4(self: *Mat4, other: *Mat4) Mat4 {
-        var out: Mat4 = Mat4.init();
-        for (0..3) |col| {
-            for (0..3) |row| {
-                for (0..3) |k| {
-                    out.elements[col * 4 + row] += self.elements[k * 4 + row] * other.elements[col * 4 + k];
-                }
-            }
-        }
+    pub fn multiplyWithVec4(self: Mat4, vec: Vec4) Vec4 {
+        return .{
+            self.col0.scale(vec.x)
+            .add(self.col1.scale(vec.y))
+            .add(self.col2.scale(vec.z))
+            .add(self.col3.scale(vec.w))
+        };
     }
 
-    pub fn multiplyVec3(self: *Mat4, vec: vec3) Mat4 {
-
+    pub fn multiplyMat4(self: Mat4, other: Mat4) Mat4 {
+        return .{
+            .col0 = self.multiplyWithVec4(other.col0),
+            .col1 = self.multiplyWithVec4(other.col1),
+            .col2 = self.multiplyWithVec4(other.col2),
+            .col3 = self.multiplyWithVec4(other.col3),
+        };
     }
 };
---
+++ zig/vec4.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+const math = std.math;
+
+pub const Vec4 = struct {
+    x: f32 = 0.0,
+    y: f32 = 0.0,
+    z: f32 = 0.0,
+    w: f32 = 0.0,
+
+    pub fn init(x: f32, y: f32, z: f32, w: f32) Vec4 {
+        return Vec4{ .x = x, .y = y, .z = z, .w = w };
+    }
+
+    pub fn magnitude(self: Vec4) f32 {
+        return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w);
+    }
+
+    pub fn dot(self: Vec4, other: Vec4) f32 {
+        return self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w;
+    }
+
+    pub fn subtract(self: Vec4, other: Vec4) Vec4 {
+        return Vec4{ .x = self.x - other.x, .y = self.y - other.y, .z = self.z - other.z, .w = self.w - other.w };
+    }
+
+    pub fn add(self: Vec4, other: Vec4) Vec4 {
+        return Vec4{ .x = self.x + other.x, .y = self.y + other.y, .z = self.z + other.z, .w = self.w + other.w };
+    }
+
+    pub fn scale(self: Vec4, s: f32) Vec4 {
+        return Vec4{
+            .x = self.x * s,
+            .y = self.y * s,
+            .z = self.z * s,
+            .w = self.w * s,
+        };
+    }
+
+    pub fn normalize(self: Vec4) Vec4 {
+        const len = self.magnitude();
+        if (len == 0.0) return self;
+        return self.scale(1.0 / len);
+    }
+};
