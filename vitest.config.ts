77b782fc6b4fe5c9837569df5772da271cc6e0ee
mocca_mo <mkdiab58@gmail.com>
2026-01-15 19:25:55 +0300

    chore(configs):
    Added config file for editorconfig consistency in .ts files
    Added config file for prettier to format .vue files
    Added config for vitest to test using playwright and using the firefox browser as the backbone

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright";
+import vue from "@vitejs/plugin-vue";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [vue(), tsconfigPaths()],
+  test: {
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [{ browser: "firefox" }],
+    },
+  },
+});
