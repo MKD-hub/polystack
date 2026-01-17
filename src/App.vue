e2505d8863e729feb8326770b8554e6b061f3bc5
mocca_mo <mkdiab58@gmail.com>
2026-01-17 20:23:10 +0300

    feat: Added general scaffolding for WebGL for Vue.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import { initCanvas, clear, loadShaderSource } from "@/scripts/webgl";
-import { loadPrefs } from "@/scripts/prefs";
-import { initShaderProgram, setupUniforms } from "@/gl";
-import TopBar from "@/layouts/TopBar.vue";
-import Footer from "@/layouts/Footer.vue";
+  import { onMounted, ref } from "vue";
+  import { initCanvas, clear } from "@/scripts/webgl";
+  import { loadPrefs } from "@/scripts/prefs";
+  import { initShaderProgram, setupUniforms } from "@/gl";
+  import TopBar from "@/layouts/TopBar.vue";
+  import Footer from "@/layouts/Footer.vue";
 
-import vsSource from "@/gl/example.vertex.glsl?raw";
-import fsSource from "@/gl/example.frag.glsl?raw";
+  import vsSource from "@/gl/example.vertex.glsl?raw";
+  import fsSource from "@/gl/example.frag.glsl?raw";
 
-const canvas = ref<HTMLCanvasElement | null>(null);
+  const canvas = ref<HTMLCanvasElement | null>(null);
 
-// @ts-expect-error: prefs is used in the template code below
-const prefs = loadPrefs();
+  // @ts-expect-error: prefs is used in the template code below
+  const prefs = loadPrefs();
 
-onMounted(async () => {
-  const gl = initCanvas(canvas.value);
+  onMounted(async () => {
+    const gl = initCanvas(canvas.value);
 
-  clear(gl);
+    clear(gl);
 
-  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-  gl.useProgram(shaderProgram);
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+    gl.useProgram(shaderProgram);
 
-  const programUniformLoc = setupUniforms(gl, shaderProgram, prefs);
+    const programUniformLoc = setupUniforms(gl, shaderProgram, prefs);
 
-  console.log(programUniformLoc);
-});
-
+    console.log(programUniformLoc);
+  });
 </script>
 
 <template>
