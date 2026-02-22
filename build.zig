6bf8b726fded4e02b1be80219391be9ffecfc8a0
mocca_mo <mkdiab58@gmail.com>
2026-02-23 01:08:07 +0300

    refactor: account for main.zig rename and export getEditorConfig

--- build.zig
+++ build.zig
@@ -3,19 +3,24 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.resolveTargetQuery(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
 
-    const wasmModule = b.addModule("wasmModule", .{ .root_source_file = b.path("zig/WASM.test.zig"), .target = target, .optimize = .ReleaseSmall });
+    const wasmModule = b.addModule("wasmModule", .{ .root_source_file = b.path("zig/main.zig"), .target = target, .optimize = .ReleaseSmall });
 
-    wasmModule.export_symbol_names = &.{ "multiplier", "getIdentity", "malloc", "free" };
+    // zig fmt: off
+    wasmModule.export_symbol_names = &.{ 
+        "malloc",
+        "free",
+        "getEditorConfig"
+    };
 
     const exe = b.addExecutable(.{
-        .name = "wasmtest",
+        .name = "main-wasm",
         .root_module = wasmModule,
     });
 
     exe.entry = .disabled;
 
     b.installArtifact(exe);
-    const installStep = b.addInstallFile(exe.getEmittedBin(), "../public/wasmtest.wasm");
+    const installStep = b.addInstallFile(exe.getEmittedBin(), "../public/polystack.wasm");
 
     b.getInstallStep().dependOn(&installStep.step);
 }
