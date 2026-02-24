faa0161aa46670cd39b5f5b2c9623ae92b3556e7
mocca_mo <mkdiab58@gmail.com>
2026-02-24 15:24:10 +0300

    chore: remove excess types

--- src/types/wasm.ts
+++ src/types/wasm.ts
@@ -1,21 +1,7 @@
 export interface WasmExports {
   memory: WebAssembly.Memory;
-  getIdentity(ptr: number): void;
-  init_vec3(array: number, x: number, y: number, z: number): void;
-  init_mat4(array: number): void;
-  multiply_mat4(array: number): void;
-  dot_vec3(vec1: number, vec2: number): number;
   malloc(size: number): number;
   free(ptr: number, size: number): void;
-  multiplier(num: number): number;
-  return_view_matrix(target: number, res: number): void;
-  return_perspective(
-    fov: number,
-    aspect: number,
-    near: number,
-    far: number,
-    res: number
-  ): void;
 
   getEditorConfig(
     fov: number,
@@ -28,4 +14,6 @@ export interface WasmExports {
   updateCamera(): void;
   returnViewMatrix(): Float32Array;
   returnPerspectiveMatrix(): Float32Array;
+  cameraRotate(theta: number, phi: number): void;
+  zoom(zoom: number): void;
 }
