2ef07a00a9d8192d65d899da362d7f1fc1771046
mocca_mo <mkdiab58@gmail.com>
2026-04-08 15:12:01 +0300

    feat: add context-menu UI with icons

--- src/components/context-menu.vue
+++ src/components/context-menu.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+  import {
+    Clapperboard,
+    BadgePlus,
+    Import,
+    ClipboardCopy,
+    ClipboardPaste,
+    Focus,
+    Scan,
+    Box,
+    Pyramid,
+    Torus,
+    Circle,
+  } from "lucide-vue-next";
   import MouseController from "@/controllers/mouse.controller.ts";
   import { defineProps, watchEffect } from "vue";
   const { mouse_controller } = defineProps<{
@@ -15,13 +28,52 @@
   <!-- get mouse pos -->
   <ul
     v-if="mouse_controller?.cm.open"
-    class="menu absolute menu-md max-w-fit bg-base-200 z-10 rounded-box"
+    class="menu w-73 absolute menu-md bg-base-200 z-10 rounded-box"
     :style="{
       top: mouse_controller?.cm.y + 'px',
       left: mouse_controller?.cm.x + 'px',
     }"
   >
-    <li><a>Medium 1</a></li>
-    <li><a>Medium 2</a></li>
+    <li>
+      <details open>
+        <summary><BadgePlus size="16" />Create</summary>
+        <ul>
+          <li>
+            <a><Box size="16" />Cube</a>
+          </li>
+          <li>
+            <a><Pyramid size="16" />Pyramid</a>
+          </li>
+          <li>
+            <a><Circle size="16" />Sphere</a>
+          </li>
+          <li>
+            <a><Torus size="16" />Torus</a>
+          </li>
+        </ul>
+      </details>
+    </li>
+    <li>
+      <details open>
+        <summary><Clapperboard size="16" />Camera Controls</summary>
+        <ul>
+          <li>
+            <a><Focus size="16" />Focus Camera on Object</a>
+          </li>
+          <li>
+            <a><Scan size="16" />Focus Camera on World Origin</a>
+          </li>
+        </ul>
+      </details>
+    </li>
+    <!-- <li> -->
+    <!--   <a><Import size="16" />Import</a> -->
+    <!-- </li> -->
+    <!-- <li> -->
+    <!--   <a><ClipboardCopy size="16" />Copy</a> -->
+    <!-- </li> -->
+    <!-- <li> -->
+    <!--   <a><ClipboardPaste size="16" />Paste</a> -->
+    <!-- </li> -->
   </ul>
 </template>
